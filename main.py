47c663963b09cefc46949d3e687316eab2c9cbd5
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-11-27 19:56:26 +0700

    added menu

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
-from telegram.ext import CommandHandler, Updater, CallbackQueryHandler
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
+from telegram import BotCommand
 from commands.today_tomorrow import send_today_tomorrow_events
 from commands.week import send_week_events
 from commands.start import start
+
+def handle_message(update, context):
+    text = update.message.text
+    if text == 'Події на сьогодні та завтра':
+        send_today_tomorrow_events(update, context)
+    elif text == 'Події на цей тиждень':
+        send_week_events(update, context)
 
 def main():
     TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -10,14 +18,18 @@ def main():
     updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    # Set bot commands for the menu
+    updater.bot.set_my_commands([
+        BotCommand("start", "Почати взаємодію з ботом"),
+        BotCommand("getevents", "Отримати події на сьогодні та завтра"),
+        BotCommand("getweekevents", "Отримати події на цей тиждень")
+    ])
+
     # Command to start the bot and show buttons
     dispatcher.add_handler(CommandHandler('start', start))
 
-    # Command to fetch today's and tomorrow's events
-    dispatcher.add_handler(CallbackQueryHandler(send_today_tomorrow_events, pattern='getevents'))
-
-    # Command to fetch all events for the week
-    dispatcher.add_handler(CallbackQueryHandler(send_week_events, pattern='getweekevents'))
+    # Handle custom keyboard inputs
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
