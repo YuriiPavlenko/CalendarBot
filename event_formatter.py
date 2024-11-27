82a23cc9faf6372fc0ffd49e246aaa0b3355b66e
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-11-27 22:03:28 +0700

    improve attendants

--- event_formatter.py
+++ event_formatter.py
@@ -13,11 +13,21 @@ def format_meeting(meeting, thailand_tz, ukraine_tz):
     start_time_ukraine = start_time.astimezone(ukraine_tz).strftime('%H:%M')
     end_time_ukraine = end_time.astimezone(ukraine_tz).strftime('%H:%M')
 
+    # Format the goal if it exists
+    goal = f"Мета: {meeting['goal']}\n" if meeting['goal'] != 'No Description' else ''
+
+    # Format attendants, excluding the organizer and formatting emails
+    attendants = [
+        f"@{attendee.split('@')[1]}" for attendee in meeting['attendants']
+        if attendee != meeting.get('organizer', {}).get('email')
+    ]
+    attendants_str = ', '.join(attendants) if attendants else 'Немає учасників'
+
     formatted_meeting = (
         f"*{meeting['summary']}*\n"
         f"Час у Таїланді: {start_time_thailand} - {end_time_thailand}\n"
         f"Час в Україні: {start_time_ukraine} - {end_time_ukraine}\n"
-        f"Мета: {meeting['goal']}\n"
-        f"Учасники: {', '.join(meeting['attendants']) if meeting['attendants'] else 'Немає учасників'}\n"
+        f"{goal}"
+        f"Учасники: {attendants_str}\n"
     )
     return formatted_meeting 
