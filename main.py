416916d72e5b2ab8bc254e1248c5179a4852955c
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-12-03 14:23:04 +0700

    fix

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import os
 import logging
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 from handlers.start import start, language_choice
-from handlers.calendar import show_meetings
-from handlers.notifications import manage_notifications
+from handlers.calendar import show_meetings, show_user_meetings
+from handlers.notifications import manage_notifications, subscribe_notifications, unsubscribe_notifications
 from handlers.meetings import create_meeting
 from utils.database import initialize_db
 
 logging.basicConfig(level=logging.INFO)
 
-def main():
+async def main():
     initialize_db()  # Initialize the database
 
     TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     # Command handlers
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CallbackQueryHandler(language_choice, pattern='^lang_'))
-    dispatcher.add_handler(CommandHandler('show_meetings', show_meetings))
-    dispatcher.add_handler(CommandHandler('manage_notifications', manage_notifications))
-    dispatcher.add_handler(CommandHandler('create_meeting', create_meeting))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CallbackQueryHandler(language_choice, pattern='^lang_'))
+    application.add_handler(CommandHandler('show_meetings', show_meetings))
+    application.add_handler(CommandHandler('show_user_meetings', show_user_meetings))
+    application.add_handler(CommandHandler('manage_notifications', manage_notifications))
+    application.add_handler(CommandHandler('subscribe_notifications', subscribe_notifications))
+    application.add_handler(CommandHandler('unsubscribe_notifications', unsubscribe_notifications))
+    application.add_handler(CommandHandler('create_meeting', create_meeting))
 
     # Start the bot
-    updater.start_polling()
-    updater.idle()
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
