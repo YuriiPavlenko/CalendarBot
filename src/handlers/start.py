0ef1fba72ee2be3e5f3b05e63575a9eadf5a9a19
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-12-10 23:14:01 +0700

    fix

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, ConversationHandler, CallbackQueryHandler, CommandHandler
-from database import SessionLocal, set_filter, set_notifications
+from ..database import SessionLocal, set_filter, set_notifications
 from localization import STRINGS
 from .notifications import ask_notification_1h, NOTIF_1H, NOTIF_15M, NOTIF_5M, NOTIF_NEW
 
