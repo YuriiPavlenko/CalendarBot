609a93a15ab0476dd6c9f4fc8dbaf16c27b48bcf
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-12-13 05:05:47 +0700

    fix

--- src/utils.py
+++ src/utils.py
@@ -25,12 +25,22 @@ def get_tomorrow_th():
 def get_rest_week_th():
     th_tz = tz.gettz(TIMEZONE_TH)
     now_th = datetime.now(th_tz)
-    weekday = now_th.weekday()
-    days_until_saturday = 5 - weekday
-    if days_until_saturday < 0:
-        days_until_saturday = 0
-    start_of_day = now_th.replace(hour=0,minute=0,second=0,microsecond=0)
-    end_of_range = start_of_day + timedelta(days=days_until_saturday)
+    
+    # Get start of today
+    start_of_day = now_th.replace(hour=0, minute=0, second=0, microsecond=0)
+    
+    # Calculate end of current week (Friday)
+    weekday = now_th.weekday()  # 0 is Monday, 4 is Friday
+    days_until_friday = 4 - weekday  # How many days until Friday
+    
+    if days_until_friday < 0:  # If it's already weekend
+        end_of_range = start_of_day  # Just return today as end
+    else:
+        # Get end of Friday
+        end_of_range = (start_of_day + timedelta(days=days_until_friday)).replace(
+            hour=23, minute=59, second=59
+        )
+    
     return start_of_day, end_of_range
 
 def get_next_week_th():
@@ -48,6 +58,32 @@ def get_end_of_next_week():
     days_ahead = 11 - now.weekday()  # Next week's Friday
     if days_ahead <= 0:
         days_ahead += 7
-    next_friday = now + timedelta(days=days_ahead)
+    next_friday = now + timedelta(days_ahead)
     end_of_next_week = next_friday.replace(hour=23, minute=59, second=59, microsecond=0)
     return end_of_next_week
+
+def convert_meeting_to_display(meeting, ua_tz='Europe/Kiev', th_tz='Asia/Bangkok'):
+    """Convert database meeting (UTC) to display format with UA and TH times."""
+    if not meeting:
+        return None
+        
+    start_utc = meeting.start_time
+    end_utc = meeting.end_time
+    
+    if start_utc.tzinfo is None:
+        start_utc = start_utc.replace(tzinfo=tz.UTC)
+    if end_utc.tzinfo is None:
+        end_utc = end_utc.replace(tzinfo=tz.UTC)
+        
+    return {
+        "id": meeting.id,
+        "title": meeting.title,
+        "start_ua": start_utc.astimezone(tz.gettz(ua_tz)),
+        "end_ua": end_utc.astimezone(tz.gettz(ua_tz)),
+        "start_th": start_utc.astimezone(tz.gettz(th_tz)),
+        "end_th": end_utc.astimezone(tz.gettz(th_tz)),
+        "attendants": meeting.attendants.split(",") if meeting.attendants else [],
+        "hangoutLink": meeting.hangoutLink,
+        "location": meeting.location,
+        "description": meeting.description,
+    }
